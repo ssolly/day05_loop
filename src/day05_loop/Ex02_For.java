d44bff36b74e3f486f6bc0ac06f12643ed1feeba
Choi.SY <sollly0829@gmail.com>
2021-07-29 10:23:17 +0900

    Ex02_For For문 예시1 추가 작성

--- src/day05_loop/Ex02_For.java
+++ src/day05_loop/Ex02_For.java
@@ -12,8 +12,22 @@ public class Ex02_For {
 		System.out.print("반복할 횟수 입력 : ");
 		repet = sc.nextInt();
 		
+		//증감식
 		for(int i=0;i<repet;i++) {
-			System.out.println(i + "반복");
+			System.out.println(i + " 반복(증감식)");
+		}
+		
+		System.out.println();
+		
+		//감소식
+		for(int i=10;i>0;i--) {
+			System.out.println(i+" 반복(감소식)");
+		}
+		
+		//2씩 증가
+		System.out.println();
+		for (int i=0; i<10; i+=2) {
+			System.out.println(i+" 반복(2증가)");
 		}
 	}
 
