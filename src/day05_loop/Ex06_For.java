0d6efc66645ef4f17db274378663524fc56a39bc
Choi.SY <sollly0829@gmail.com>
2021-07-29 13:10:16 +0900

    Ex06_For 이중반복문2

---
+++ src/day05_loop/Ex06_For.java
@@ -0,0 +1,14 @@
+package day05_loop;
+
+public class Ex06_For {
+
+	public static void main(String[] args) {
+		
+		int n1=0,n2=0;
+		for(n1=1;n1<=5;n1++) {
+			for (n2=1;n2<=5;n2++) {
+				System.out.println("n1 : "+n1+" n2 : "+ n2);
+			}
+		}
+	}
+}
